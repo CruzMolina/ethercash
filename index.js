93c0ed125adae4c84995ddf6acd42afbb7733719
freethinkingaway <cruz.adam.molina@gmail.com>
2018-08-18 12:55:06 -0700

    added getLatestBlockTxs, getBlockTxs, getLatestBlockTxByIndex, & getBlockTxByIndex

--- index.js
+++ index.js
@@ -4,17 +4,49 @@ const web3 = new Web3(new Web3.providers.HttpProvider(infuraNode));
 
 function getLatestBlock() {
   web3.eth.getBlock("latest").then(function(blockData) {
-    console.log(blockData);
+    //console.log(blockData);
     return blockData;
   });
 }
 
 function getBlock(blockNumber) {
   web3.eth.getBlock(blockNumber).then(function(blockData) {
-    console.log(blockData);
+    //console.log(blockData);
     return blockData;
   });
 }
 
-getLatestBlock();
-getBlock(0);
+function getLatestBlockTxs() {
+  web3.eth.getBlock("latest").then(function(blockData) {
+    //console.log(blockData.transactions);
+    return blockData.transactions;
+  });
+}
+
+function getBlockTxs(blockNumber) {
+  web3.eth.getBlock(blockNumber).then(function(blockData) {
+    //console.log(blockData.transactions);
+    return blockData.transactions;
+  });
+}
+
+function getLatestBlockTxByIndex(index) {
+  web3.eth.getBlock("latest").then(function(blockData) {
+    //console.log(blockData.transactions[index]);
+    return blockData.transactions[index];
+  });
+}
+
+function getBlockTxByIndex(blockNumber, index) {
+  web3.eth.getBlock(blockNumber).then(function(blockData) {
+    //console.log(blockData.transactions[index]);
+    return blockData.transactions[index];
+  });
+}
+
+//getLatestBlock();
+//getBlock(0);
+//getLatestBlockTxs();
+//getBlockTxs(5000001);
+//getLatestBlockTxsByIndex(0);
+//getBlockTxsByIndex(5000001, 0);
