014f8121f7b2c64f183b947ae0f94bec1d9e4c70
freethinkingaway <cruz.adam.molina@gmail.com>
2018-08-18 13:47:12 -0700

    refactored exist funcs, added getBlocksByRange & getBlocksByRangeFromLatest

--- index.js
+++ index.js
@@ -3,50 +3,61 @@ const infuraNode = "https://mainnet.infura.io/";
 const web3 = new Web3(new Web3.providers.HttpProvider(infuraNode));
 
 function getLatestBlock() {
-  web3.eth.getBlock("latest").then(function(blockData) {
-    //console.log(blockData);
-    return blockData;
-  });
+  let blockData = web3.eth.getBlock("latest");
+  return blockData;
 }
 
 function getBlock(blockNumber) {
-  web3.eth.getBlock(blockNumber).then(function(blockData) {
-    //console.log(blockData);
-    return blockData;
-  });
+  let blockData = web3.eth.getBlock(blockNumber);
+  return blockData;
 }
 
-function getLatestBlockTxs() {
-  web3.eth.getBlock("latest").then(function(blockData) {
-    //console.log(blockData.transactions);
-    return blockData.transactions;
-  });
+async function getLatestBlockTxs() {
+  let blockData = await getLatestBlock();
+  return blockData.transactions;
 }
 
-function getBlockTxs(blockNumber) {
-  web3.eth.getBlock(blockNumber).then(function(blockData) {
-    //console.log(blockData.transactions);
-    return blockData.transactions;
-  });
+async function getBlockTxs(blockNumber) {
+  let blockData = await getBlock(blockNumber);
+  return blockData.transactions;
 }
 
-function getLatestBlockTxByIndex(index) {
-  web3.eth.getBlock("latest").then(function(blockData) {
-    //console.log(blockData.transactions[index]);
-    return blockData.transactions[index];
-  });
+async function getLatestBlockTxByIndex(index) {
+  let blockData = await getLatestBlock();
+  return blockData.transactions[index];
 }
 
-function getBlockTxByIndex(blockNumber, index) {
-  web3.eth.getBlock(blockNumber).then(function(blockData) {
-    //console.log(blockData.transactions[index]);
-    return blockData.transactions[index];
-  });
+async function getBlockTxByIndex(blockNumber, index) {
+  let blockData = await getBlock(blockNumber);
+  return blockData.transactions[index];
+}
+
+async function getBlocksByRange(startBlockNumber, endBlockNumber) {
+  var BlockRange = [];
+
+  for (var i = startBlockNumber; i <= endBlockNumber; i++) {
+    let blockData = await getBlock(i);
+    BlockRange.push(blockData);
+  }
+  return BlockRange;
+}
+
+async function getBlocksByRangeFromLatest(blocksFromLatest) {
+  var BlockRange = [];
+  let latestBlock = await getLatestBlock();
+
+  for (var i = blocksFromLatest; i >= 0; i--) {
+    let blockData = await getBlock(latestBlock.number - i);
+    BlockRange.push(blockData);
+  }
+  return BlockRange;
 }
 
 //getLatestBlock();
 //getBlock(0);
 //getLatestBlockTxs();
 //getBlockTxs(5000001);
-//getLatestBlockTxsByIndex(0);
-//getBlockTxsByIndex(5000001, 0);
+//getLatestBlockTxByIndex(0);
+//getBlockTxByIndex(5000001, 0);
+//getBlocksByRange(0, 5);
+//getBlocksByRangeFromLatest(5);
