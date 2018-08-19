0234a55cca5f2bf44d41ee6c886a418b88a760e5
freethinkingaway <cruz.adam.molina@gmail.com>
2018-08-18 22:32:54 -0700

    renamed parseTxData to parseNullTxData, added parseToNumberValue, refactored getLatestBlockEtherTransferred, added getBlockEtherTransferred, getBlockRangeEtherTransferred, getBlockRangeFromLatestEtherTransferred, parseTxValue, parseTxRecipient, & getLatestBlockRecipientsOfEtherTransferred

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ async function getBlockTxs(blockNumber) {
 
 async function getLatestBlockTxByIndex(index) {
   let blockData = await getLatestBlock();
-  console.log(blockData.transactions[index]);
+  //console.log(blockData.transactions[index]);
   return blockData.transactions[index];
 }
 
@@ -60,7 +60,7 @@ async function getTxData(txHash) {
   return txData;
 }
 
-function parseTxData(txData) {
+function parseNullTxData(txData) {
   if (txData !== null) {
     return txData.value;
   } else {
@@ -68,21 +68,94 @@ function parseTxData(txData) {
   }
 }
 
-async function getLatestEtherTransferred() {
+function parseToNumberValue(parsedValue) {
+  let stringEtherValue = web3.utils.fromWei(parsedValue);
+  let numberValue = Number(stringEtherValue);
+  return numberValue;
+}
+
+async function getLatestBlockEtherTransferred() {
   let latestTxs = await getLatestBlockTxs();
-  let latestTxValues = [];
-  var latestTxData = [];
   let totalValue = 0;
 
   for (const tx of latestTxs) {
     let txData = await getTxData(tx);
-    let parsedTxValue = await parseTxData(txData);
-    let stringEtherValue = await web3.utils.fromWei(parsedTxValue);
-    let numberValue = Number(stringEtherValue);
+    let parsedTxValue = await parseNullTxData(txData);
+    let numberValue = await parseToNumberValue(parsedTxValue);
     totalValue += numberValue;
+  }
+  //console.log(totalValue);
+  return totalValue;
+}
+
+async function getBlockEtherTransferred(blockNumber) {
+  let BlockTxs = await getBlockTxs(blockNumber);
+  let totalValue = 0;
+
+  for (const tx of BlockTxs) {
+    let txData = await getTxData(tx);
+    let parsedTxValue = await parseNullTxData(txData);
+    let numberValue = await parseToNumberValue(parsedTxValue);
+    totalValue += numberValue;
+  }
+  //console.log(totalValue);
+  return totalValue;
+}
+
+async function getBlockRangeEtherTransferred(startBlockNumber, endBlockNumber) {
+  let BlockRange = await getBlocksByRange(startBlockNumber, endBlockNumber);
+  let totalValue = 0;
+
+  for (const block of BlockRange) {
+    let blockValue = await getBlockEtherTransferred(block.number);
+    totalValue += blockValue;
   }
   console.log(totalValue);
   return totalValue;
+}
+
+async function getBlockRangeFromLatestEtherTransferred(blocksFromLatest) {
+  let BlockRange = await getBlocksByRangeFromLatest(blocksFromLatest);
+  let totalValue = 0;
+
+  for (const block of BlockRange) {
+    let blockValue = await getBlockEtherTransferred(block.number);
+    totalValue += blockValue;
+  }
+  console.log(totalValue);
+  return totalValue;
+}
+
+function parseTxValue(txData) {
+  let parsedTxValue = parseNullTxData(txData);
+  let numberValue = parseToNumberValue(parsedTxValue);
+  return numberValue;
+}
+
+function parseTxRecipient(txData) {
+  let recipient = txData.to;
+  return recipient;
+}
+
+async function getLatestBlockRecipientsOfEtherTransferred() {
+  let latestTxs = await getLatestBlockTxs();
+  let recipients = {};
+
+  for (const tx of latestTxs) {
+    let txData = await getTxData(tx);
+    let numberValue = await parseTxValue(txData);
+    if (numberValue > 0) {
+      let txRecipient = await parseTxRecipient(txData);
+      //console.log(numberValue);
+      recipients[txRecipient] = numberValue + 0 || 0;
+      //console.log(recipients[txRecipient]);
+      //amounts.push(numberValue);
+      //totalValue += numberValue;
+    }
+  }
+  //console.log(recipients);
+  //console.log(amounts);
+  return recipients;
 }
 
 //getLatestBlock();
@@ -93,7 +166,9 @@ async function getLatestEtherTransferred() {
 //getBlockTxByIndex(5000001, 0);
 //getBlocksByRange(0, 5);
 //getBlocksByRangeFromLatest(5);
-
 //getTxData("0xed0c9a5e4b75cc10e9e0e8106ab5c3c16381582e20b8ee644b99d98fb68fb36f");
-
-getLatestEtherTransferred();
+//getLatestBlockEtherTransferred();
+//getBlockEtherTransferred(5000001);
+//getBlockRangeEtherTransferred(5000000, 5000005);
+//getBlockRangeFromLatestEtherTransferred(5);
+//getLatestBlockRecipientsOfEtherTransferred();
